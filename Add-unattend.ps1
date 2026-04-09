faa857fb858149648bfdb95b1b13027dd6abf9a8
uruktek <cody@uruktek.com>
2026-04-09 16:17:32 -0500

    ppkg testing

--- Add-unattend.ps1
+++ Add-unattend.ps1
@@ -4,7 +4,7 @@ $Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Add-unattend.l
 Start-Transcript -Path (Join-Path "C:\OSDCloud\Logs\" $Global:Transcript) -ErrorAction Ignore
 
 try {
-    Invoke-WebRequest -Uri https://raw.githubusercontent.com/uruktek/OSDCloud/main/unattend.xml -OutFile Out-File -FilePath 'C:\Windows\unattend.xml' -Encoding ascii -Force
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/uruktek/OSDCloud/main/unattend.xml -OutFile Out-File -FilePath 'C:\Windows\Panther\unattend.xml' -Encoding ascii -Force
     Write-host -ForegroundColor Green "unattend.xml has been downloaded to C:\Windows\Panther\unattend.xml"
 }
 catch {
